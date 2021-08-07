f8ed5d39b67425e78a9348d1bb6186082c7893de
tonytran0314 <tonytran0314@gmail.com>
2021-08-07 00:50:11 -0400

    Delete some unnecessary notes

--- motor.py
+++ motor.py
@@ -11,43 +11,6 @@ pwm = GPIO.PWM(Ena, 100)
 pwm.start(0)
 
 sleepTime = 3
-'''
-#counterclockwise
-#increasing speed
-for i in range(0, 100, 10):
-    GPIO.output(In1, GPIO.HIGH)
-    GPIO.output(In2, GPIO.LOW)
-    pwm.ChangeDutyCycle(i)
-    print(i)
-    sleep(sleepTime)
-    
-#decreasing speed
-for j in range(100, -10, -10):
-    GPIO.output(In1, GPIO.HIGH)
-    GPIO.output(In2, GPIO.LOW)
-    pwm.ChangeDutyCycle(j)
-    print(j)
-    sleep(sleepTime)
-    
-#clockwise
-#increasing speed
-for i in range(0, 100, 10):
-    GPIO.output(In1, GPIO.LOW)
-    GPIO.output(In2, GPIO.HIGH)
-    pwm.ChangeDutyCycle(i)
-    print(i)
-    sleep(sleepTime)
-    
-#decreasing speed
-for j in range(100, -10, -10):
-    GPIO.output(In1, GPIO.LOW)
-    GPIO.output(In2, GPIO.HIGH)
-    pwm.ChangeDutyCycle(j)
-    print(j)
-    sleep(sleepTime)
-    
-exit()
-'''
 
 GPIO.output(In1, GPIO.HIGH)
 GPIO.output(In2, GPIO.LOW)
