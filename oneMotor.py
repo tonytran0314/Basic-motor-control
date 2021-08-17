80838464a4703825f1772559a7e67301fa2df5d9
Huy Tran <55000985+tonytran0314@users.noreply.github.com>
2021-08-16 20:50:57 -0400

    Change sleep time and add breakTime

--- oneMotor.py
+++ oneMotor.py
@@ -12,7 +12,8 @@ GPIO.setup(In1,GPIO.OUT)
 GPIO.setup(In2,GPIO.OUT)
 pwm = GPIO.PWM(Ena, 100)
 
-sleepTime = 3
+workingTime = 0.5
+breakTime = 3
 
 pwm.start(0)
 
@@ -22,18 +23,18 @@ try:
         GPIO.output(In1, GPIO.HIGH)
         GPIO.output(In2, GPIO.LOW)
         pwm.ChangeDutyCycle(100)
-        sleep(sleepTime)
+        sleep(workingTime)
 
         pwm.ChangeDutyCycle(0)
-        sleep(sleepTime)
+        sleep(breakTime)
 
         GPIO.output(In1, GPIO.LOW)
         GPIO.output(In2, GPIO.HIGH)
         pwm.ChangeDutyCycle(100)
-        sleep(sleepTime)
+        sleep(workingTime)
         
         pwm.ChangeDutyCycle(0)
-        sleep(sleepTime)
+        sleep(breakTime)
 
 except KeyboardInterrupt:
     print("\nThe program has been stopped")
