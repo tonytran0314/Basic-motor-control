e1fe94562572c4cf2dd7bfa5de8785307d07cab4
Huy Tran <55000985+tonytran0314@users.noreply.github.com>
2021-08-17 11:53:32 -0400

    Add try except finally

--- keyboardMotor.py
+++ keyboardMotor.py
@@ -33,44 +33,49 @@ pwmB.start(0)
 sleepTime = 0.25
 moveMent = None
 
-while True:
-    #Wait for user to enter a letter
-    moveMent = raw_input("Enter w, s or x: ") 
+try:
+    while True:
+        #Wait for user to enter a letter
+        moveMent = raw_input("Enter w, s or x: ") 
 
-    # if user enters s or S, motor 1 rotates clockwise, the other rotates counter-clockwise
-    if moveMent == "s" or moveMent == "S": 
-        print("Backward")
-        
-        # 1 low 1 high so the motor can spin
-        GPIO.output(In1, GPIO.LOW)
-        GPIO.output(In2, GPIO.HIGH)
-        pwmA.ChangeDutyCycle(100) # set motor speed to 100%
-        GPIO.output(In3, GPIO.HIGH)
-        GPIO.output(In4, GPIO.LOW)
-        pwmB.ChangeDutyCycle(100)
-        sleep(sleepTime)
+        # if user enters s or S, motor 1 rotates clockwise, the other rotates counter-clockwise
+        if moveMent == "s" or moveMent == "S": 
+            print("Backward")
 
-    # if user enters w or W, motor 2 rotates clockwise, the other rotates counter-clockwise
-    elif moveMent == "w" or moveMent == "W":
-        print("Forward")
-        GPIO.output(In1, GPIO.HIGH)
-        GPIO.output(In2, GPIO.LOW)
-        pwmA.ChangeDutyCycle(100)
-        GPIO.output(In3, GPIO.LOW)
-        GPIO.output(In4, GPIO.HIGH)
-        pwmB.ChangeDutyCycle(100)
-        sleep(sleepTime)
+            # 1 low 1 high so the motor can spin
+            GPIO.output(In1, GPIO.LOW)
+            GPIO.output(In2, GPIO.HIGH)
+            pwmA.ChangeDutyCycle(100) # set motor speed to 100%
+            GPIO.output(In3, GPIO.HIGH)
+            GPIO.output(In4, GPIO.LOW)
+            pwmB.ChangeDutyCycle(100)
+            sleep(sleepTime)
 
-    # x or X to stop the motors
-    elif moveMent == "x" or moveMent == "X":
-        print("Motors have been stopped")
-        pwmA.ChangeDutyCycle(0)
-        pwmB.ChangeDutyCycle(0)
-        sleep(sleepTime)
-        
-    # z or Z to exit the program
-    elif moveMent == "z" or moveMent == "Z":
-        print("Exit")
-        exit()
-    else:
-        print("Only w, s or x")
+        # if user enters w or W, motor 2 rotates clockwise, the other rotates counter-clockwise
+        elif moveMent == "w" or moveMent == "W":
+            print("Forward")
+            GPIO.output(In1, GPIO.HIGH)
+            GPIO.output(In2, GPIO.LOW)
+            pwmA.ChangeDutyCycle(100)
+            GPIO.output(In3, GPIO.LOW)
+            GPIO.output(In4, GPIO.HIGH)
+            pwmB.ChangeDutyCycle(100)
+            sleep(sleepTime)
+
+        # x or X to stop the motors
+        elif moveMent == "x" or moveMent == "X":
+            print("Motors have been stopped")
+            pwmA.ChangeDutyCycle(0)
+            pwmB.ChangeDutyCycle(0)
+            sleep(sleepTime)
+
+        # z or Z to exit the program
+        elif moveMent == "z" or moveMent == "Z":
+            print("Exit")
+            exit()
+        else:
+            print("Only w, s or x")
+except KeyboardInterrupt:
+    print("\nThe program has been stopped")
+finally:
+    GPIO.cleanup()
