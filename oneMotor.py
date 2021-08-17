e1e9daf5f2e3c8b82d258f07dd250cd68dba7b92
Huy Tran <55000985+tonytran0314@users.noreply.github.com>
2021-08-16 20:56:17 -0400

    Change workingTime to 0..5 and speed to 40

--- oneMotor.py
+++ oneMotor.py
@@ -12,7 +12,7 @@ GPIO.setup(In1,GPIO.OUT)
 GPIO.setup(In2,GPIO.OUT)
 pwm = GPIO.PWM(Ena, 100)
 
-workingTime = 0.1
+workingTime = 0.05
 breakTime = 3
 
 pwm.start(0)
@@ -22,7 +22,7 @@ try:
     while True:
         GPIO.output(In1, GPIO.HIGH)
         GPIO.output(In2, GPIO.LOW)
-        pwm.ChangeDutyCycle(25)
+        pwm.ChangeDutyCycle(40)
         sleep(workingTime)
 
         pwm.ChangeDutyCycle(0)
@@ -30,7 +30,7 @@ try:
 
         GPIO.output(In1, GPIO.LOW)
         GPIO.output(In2, GPIO.HIGH)
-        pwm.ChangeDutyCycle(25)
+        pwm.ChangeDutyCycle(40)
         sleep(workingTime)
         
         pwm.ChangeDutyCycle(0)
