1681538fc825d34f03d0c1738fd1aaa836f855cd
tonytran0314 <tonytran0314@gmail.com>
2021-08-16 20:19:14 -0400

    Add try except finally

--- oneMotor.py
+++ oneMotor.py
@@ -9,21 +9,28 @@ GPIO.setup(Ena,GPIO.OUT)
 GPIO.setup(In1,GPIO.OUT)
 GPIO.setup(In2,GPIO.OUT)
 pwm = GPIO.PWM(Ena, 100)
+
 pwm.start(0)
 
 sleepTime = 3
 
-GPIO.output(In1, GPIO.HIGH)
-GPIO.output(In2, GPIO.LOW)
-pwm.ChangeDutyCycle(100)
-sleep(sleepTime)
+try:
+    while True:
+        GPIO.output(In1, GPIO.HIGH)
+        GPIO.output(In2, GPIO.LOW)
+        pwm.ChangeDutyCycle(100)
+        sleep(sleepTime)
 
-pwm.ChangeDutyCycle(0)
-sleep(sleepTime)
+        pwm.ChangeDutyCycle(0)
+        sleep(sleepTime)
 
-GPIO.output(In1, GPIO.LOW)
-GPIO.output(In2, GPIO.HIGH)
-pwm.ChangeDutyCycle(100)
-sleep(sleepTime)
+        GPIO.output(In1, GPIO.LOW)
+        GPIO.output(In2, GPIO.HIGH)
+        pwm.ChangeDutyCycle(100)
+        sleep(sleepTime)
 
-exit()
+except KeyboardInterrupt:
+    print("The program has been stopped")
+
+finally:
+    GPIO.cleanup()
