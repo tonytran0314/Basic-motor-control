773d3a8c01e4a2b44f895aba2b2dc7a5db985027
Huy Tran <55000985+tonytran0314@users.noreply.github.com>
2021-08-17 23:11:49 -0400

    Add notes

--- oneMotor.py
+++ oneMotor.py
@@ -2,32 +2,41 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+# Set mode and turn warnings off
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+# Pins declaration
 Ena, In1, In2 = 17,27,22
 
+# Declare output
 GPIO.setup(Ena,GPIO.OUT)
 GPIO.setup(In1,GPIO.OUT)
 GPIO.setup(In2,GPIO.OUT)
 pwm = GPIO.PWM(Ena, 100)
 
+# Declare workingTime and breakTime
 workingTime = 0.05
 breakTime = 3
 
+# Motor stop before running
 pwm.start(0)
 
 
 try:
     while True:
+        # Motor rotate
         GPIO.output(In1, GPIO.HIGH)
         GPIO.output(In2, GPIO.LOW)
-        pwm.ChangeDutyCycle(40)
+        # Motor speed: 40%
+        pwm.ChangeDutyCycle(40) 
         sleep(workingTime)
 
+        # Motor stop completely (speed = 0)
         pwm.ChangeDutyCycle(0)
         sleep(breakTime)
-
+        
+        # Motor rotate reversely
         GPIO.output(In1, GPIO.LOW)
         GPIO.output(In2, GPIO.HIGH)
         pwm.ChangeDutyCycle(40)
@@ -36,8 +45,10 @@ try:
         pwm.ChangeDutyCycle(0)
         sleep(breakTime)
 
+# print this when press Ctrl C
 except KeyboardInterrupt:
     print("\nThe program has been stopped")
 
+# clean up all status before end the program
 finally:
     GPIO.cleanup()
